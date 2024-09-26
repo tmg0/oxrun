3ed79caab346d6b0ef739b6854aaa3e95c5e596e
Tamago <zekun.jin@foxmail.com>
2024-09-26 10:29:57 +0800

    feat: insert scripts

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,23 @@
 import { execa } from 'execa'
 import mri from 'mri'
 import process from 'node:process'
-import { resolve } from 'node:path'
 import { resolve as resolveImport } from 'import-meta-resolve'
 
 const argv = process.argv.slice(2)
-const { _: entries } = mri(argv)
+const { _: scripts } = mri(argv)
 
 function runNodeCommand(args: (string | string[])[] = []) {
   return execa('node', args.flat(), { stdio: 'inherit' })
 }
 
 export function main() {
-  if (!entries.length)
+  if (!scripts.length)
     return
 
-  const root = process.cwd()
   const path = resolveImport('@oxc-node/core/register', import.meta.url)
 
-  for (const entry of entries) {
-    runNodeCommand([
-      ['--import', path],
-      resolve(root, entry)
-    ])
-  }
+  runNodeCommand([
+    ['--import', path],
+    scripts
+  ])
 }
