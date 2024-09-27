5b1636e16123a54f4a43b815c2ebb90f15ccbe7a
Tamago <zekun.jin@foxmail.com>
2024-09-27 17:26:52 +0800

    refactor: oxrun

---
+++ src/core/oxrun.ts
@@ -0,0 +1,39 @@
+import type { Options } from './types'
+import { promises as fs } from 'node:fs'
+import { transform } from '@oxc-node/core'
+import { nanoid } from 'nanoid'
+import { createContext } from './context'
+
+export const oxrun = Object.assign(
+  async (scripts: string | string[]) => {
+    const options: Options = { scripts: [scripts].flat(), watch: false }
+    const ctx = createContext(options)
+    await ctx.run()
+  },
+
+  {
+    async transform(id: string, code?: string) {
+      if (!code)
+        code = await fs.readFile(id, 'utf8')
+      const output = transform(id, code)
+
+      return {
+        get code() {
+          return output.source()
+        },
+        get map() {
+          return output.sourceMap()
+        },
+      }
+    },
+
+    async import<T = any>(id: string) {
+      const { code } = await this.transform(id)
+      const outfile = `./oxrun.${nanoid()}.mjs`
+      await fs.writeFile(outfile, code, 'utf8')
+      const mod = await import(outfile)
+      fs.unlink(outfile)
+      return mod as T
+    },
+  },
+)
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
-import type { Options } from './core/types'
-import { promises as fs } from 'node:fs'
-import { transform } from '@oxc-node/core'
-import { nanoid } from 'nanoid'
 import { createContext } from './core/context'
 import { resolveOptions } from './core/options'
+import { oxrun } from './core/oxrun'
 
 export async function main() {
   const options = resolveOptions()
@@ -12,38 +9,6 @@ export async function main() {
   await ctx.run()
 }
 
-export const oxrun = Object.assign(
-  async (scripts: string | string[]) => {
-    const options: Options = { scripts: [scripts].flat(), watch: false }
-    const ctx = createContext(options)
-    await ctx.run()
-  },
-
-  {
-    async transform(id: string, code?: string) {
-      if (!code)
-        code = await fs.readFile(id, 'utf8')
-      const output = transform(id, code)
-
-      return {
-        get code() {
-          return output.source()
-        },
-        get map() {
-          return output.sourceMap()
-        },
-      }
-    },
-
-    async import<T = any>(id: string) {
-      const { code } = await this.transform(id)
-      const outfile = `./oxrun.${nanoid()}.mjs`
-      await fs.writeFile(outfile, code, 'utf8')
-      const mod = await import(outfile)
-      fs.unlink(outfile)
-      return mod as T
-    },
-  },
-)
+export { oxrun }
 
 export default oxrun
